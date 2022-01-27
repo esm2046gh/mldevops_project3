87229b8723cc898af0a3acdcd75aac191357b0e8
esm <esm@mymail.com>
2022-01-27 18:29:33 +0100

    starter/__init__.py renamed to __init__.py_not_used

--- starter/main.py
+++ starter/main.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
-from starter.starter.ml.model import inference
-from starter.starter.ml.data import process_data
-from starter.starter.ml.data import Dump
+from starter.ml.model import inference
+from starter.ml.data import process_data
+from starter.ml.data import Dump
 
 import os
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
